1f66a3f89fc0b33e4bd0f22ce78b393f9295c342
Matt Hagner <web@matthagner.com>
2019-09-10 20:16:27 -0500

    Implement POST for register

--- src/routes/register.js
+++ src/routes/register.js
@@ -1,6 +1,37 @@
 const router = require("express").Router();
+const User = require("../controllers/users");
 
-router.get("/", (req, res) => {
+function validateUserInput(req, res, next) {
+  const { username, password } = req.body;
+
+  if (!username || !password) {
+    res.status(400).json({
+      message: "Expected username and password"
+    });
+  } else {
+    next();
+  }
+}
+
+router.post("/", validateUserInput, async (req, res) => {
+  const { username, password } = req.body;
+
+  try {
+    const user = await User.createUser({ username, password });
+    console.log(user);
+
+    res.status(201).json({
+      user
+    });
+  } catch (err) {
+    res.status(500).json({
+      error: "Internal server error",
+      message: err.message
+    });
+  }
+});
+
+router.get("/", (_req, res) => {
   res.json({
     message: "get /api/register"
   });
